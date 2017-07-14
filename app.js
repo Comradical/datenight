7acbeef8ce37fedf9d2757f89d72c31fa76d3ad0
Brian Austgen <linguistic151@gmail.com>
2017-07-14 01:51:49 +0000

    update all routes hooks, and initial UI updates

--- app.js
+++ app.js
@@ -12,17 +12,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//Mongoose / Model Config
-// var personSchema = new mongoose.Schema({
-//     firstname: String,
-//     lastname: String,
-//     compare: String,
-//     answers: Object,
-//     created: {type: Date, default: Date.now}
-// });
-
-// var Person = mongoose.model("Person", personSchema);
-
 var activities = ["Painting", "Tennis", "Volleyball", "Broadway", "Hiking", "Swimming" ];
 
 //Home Page
@@ -35,38 +24,8 @@ app.get("/results/:id", resultHook);
 
 app.post("/people", peopleHook);
 
-// function(req, res){
-//     var finalAnswers = {};
-//     var answers = req.body.answers;
-//     var x;
-//         for (x in answers) {
-//              if (answers.hasOwnProperty(x)) {
-//         // do stuff
-//         finalAnswers[x] = parseInt(answers[x])
-        
-//             }
-//         }
-    
-//     Person.create({
-//         firstname: req.body.person.firstname,
-//         lastname: req.body.person.lastname,
-//         compare: req.body.person.compare,
-//         answers: finalAnswers,
-        
-//     }, function(err, newPerson){
-//         if(err){
-//             console.log(err);
-//             res.render("/");
-//         } else {
-//             res.redirect("/results/"+newPerson._id);
-//         }
-//     });
-// };
-
-
-
 //Serve the app on Cloud 9 Port 
-//https://datenight-linguistic151.c9users.io
+//https://datenight-linguistic151.c9users.io/?compare=58d1949c66d4480ca936fed7
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Your Datenight has commenced!");
 });
--- hooks/result-hook.js
+++ hooks/result-hook.js
@@ -1,21 +1,15 @@
 var personService   = require("../services/person-service.js");
 var activities = ["Painting", "Tennis", "Volleyball", "Broadway", "Hiking", "Swimming" ];
     
-    // personService.findById(req.params.id, function(err, mainPerson, secondPerson){
-    //      if(err){
-    //         console.log(err);
-    //     }
-    //     res.render("results", {activities: activities, person: mainPerson, personTwo: secondPerson,});
-    // });
-    
 module.exports = function(req, res){
-    
-    console.log("from result hook + " + req.params.id);
     personService.findById(req.params.id, function(err, mainPerson, secondPerson){
         if(err){
             console.log(err);
+        } else if(secondPerson){
+        res.render("results", {activities: activities, person: mainPerson, personTwo: secondPerson,}); 
         } else {
-       res.render("results", {activities: activities, person: mainPerson, personTwo: secondPerson,}); 
+        res.render("singleResults", {activities: activities, person: mainPerson, personTwo: secondPerson,});    
         }
+        
     });
 };
